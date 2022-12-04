1583063ec99da8b3e9dad6f89f8ff6e98e2c6b34
Mario Muñiz <mario@mariomuniz.com>
2022-12-04 01:24:45 -0800

    do not display thumbnail if no image

--- apps/web/src/components/blog-post-preview.js
+++ apps/web/src/components/blog-post-preview.js
@@ -15,18 +15,20 @@ function BlogPostPreview(props) {
       className={props.isInList ? styles.inList : styles.inGrid}
       to={getBlogUrl(props.publishedAt, props.slug.current)}
     >
-      <div className={styles.leadMediaThumb}>
-        {props.mainImage && props.mainImage.asset && (
-          <img
-            src={imageUrlFor(buildImageObj(props.mainImage))
-              .width(600)
-              .height(Math.floor((9 / 16) * 600))
-              .auto('format')
-              .url()}
-            alt={props.mainImage.alt}
-          />
-        )}
-      </div>
+      {props.mainImage && (
+        <div className={styles.leadMediaThumb}>
+          {props.mainImage && props.mainImage.asset && (
+            <img
+              src={imageUrlFor(buildImageObj(props.mainImage))
+                .width(600)
+                .height(Math.floor((9 / 16) * 600))
+                .auto('format')
+                .url()}
+              alt={props.mainImage.alt}
+            />
+          )}
+        </div>
+      )}
       <div className={styles.text}>
         <h3 className={cn(responsiveTitle3, styles.title)}>{props.title}</h3>
         {props._rawExcerpt && (
